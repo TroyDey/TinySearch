d23d543e73ae40c7873fadbdf93b82ac262e121a
Troy Dey <applyevalloop@gmail.com>
2016-08-28 22:31:10 -0500

    Remove printing should only transform results into their output format

--- TinySearch/ResultHandler.fs
+++ TinySearch/ResultHandler.fs
@@ -9,11 +9,9 @@ module ResultHandler =
     type outputResult = { doc: string; score: double; termHitCount: int; maxTerms: int; coordScore: double; debug: scoreDebug list }
 
     //just return card json string and debug info potentially take other transformers
-    let outputResults resultPrinter debugPrinter (page:pagination) (results:scoredResult list) =
+    let outputResults (page:pagination) (results:scoredResult list) =
         results 
         |> List.skip (page.pageIdx * page.rows) 
         |> List.take page.rows
         //coordination.score and coordination.maxTerms should be guaranteed to have a value of at least 1 by this point 
         |> List.map (fun r -> { doc = (getDocument r.docId); score = r.score; termHitCount = r.coordination.termHitCount; maxTerms = r.coordination.maxTerms.Value; coordScore = r.coordination.score.Value; debug = r.debug })
-        |> List.map (fun outRes -> (resultPrinter outRes; outRes)) 
-        |> List.map (fun outRes -> (debugPrinter outRes; outRes))
--- TinySearch/ResultHandler.fsi
+++ TinySearch/ResultHandler.fsi
@@ -7,4 +7,4 @@ module ResultHandler =
     type pagination = { pageIdx: int; rows: int }
     type outputResult = { doc: string; score: double; termHitCount: int; maxTerms: int; coordScore: double; debug: scoreDebug list }
 
-    val outputResults : (outputResult -> unit) -> (outputResult -> unit) -> pagination -> scoredResult list -> outputResult list
+    val outputResults : pagination -> scoredResult list -> outputResult list
