1ec2f3765bd6381e520344ed6e7803e3de29db5a
Troy Dey <applyevalloop@gmail.com>
2016-09-21 22:16:44 -0500

    Fix bug with page size being larger then results size.

--- TinySearch/ResultHandler.fs
+++ TinySearch/ResultHandler.fs
@@ -9,6 +9,6 @@ module ResultHandler =
     let outputResults (page:pagination) (results:scoredResult list) =
         results 
         |> List.skip (page.pageIdx * page.rows) 
-        |> List.take page.rows
+        |> List.take (if page.rows > results.Length then results.Length else page.rows)
         //coordination.score and coordination.maxTerms should be guaranteed to have a value of at least 1 by this point 
         |> List.map (fun r -> { doc = (getDocument r.docId); score = r.score; termHitCount = r.coordination.termHitCount; maxTerms = r.coordination.maxTerms.Value; coordScore = r.coordination.score.Value; debug = r.debug })
